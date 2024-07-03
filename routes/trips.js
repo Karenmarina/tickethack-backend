658c2b8b12bf0557623a3c206a2bab3d0dbeba97
Karenmarina <kareen_92@hotmail.fr>
2024-07-03 09:56:30 +0200

    Route post search

---
+++ routes/trips.js
@@ -0,0 +1,31 @@
+require("../models/connection");
+const Trip = require("../models/trips");
+
+var express = require("express");
+var router = express.Router();
+
+router.post("/", (req, res) => {
+  Trip.find({
+    departure: { $regex: new RegExp(req.body.departure, "i") },
+    arrival: { $regex: new RegExp(req.body.arrival, "i") },
+    date: {
+      $gte: new Date(req.body.date),
+      $lt: new Date(
+        new Date(req.body.date).setDate(
+          new Date(req.body.date).getDate() + 1,
+          0,
+          0,
+          0
+        )
+      ),
+    },
+  }).then((data) => {
+    if (this.data) {
+      res.json({ result: true, allTrips: data });
+    } else {
+      res.json({ result: false, error: "No trip found" });
+    }
+  });
+});
+
+module.exports = router;
